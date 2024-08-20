7dda3cfc86d09d5541ecb1088104d471d3aa764c
Byron Duarte <byronduarte95@gmail.com>
2024-08-19 23:54:16 -0500

    🔥 setup: configured ESLint, Jest, Prettier, next-seo, and Husky for code linting
    Configured ESLint, Prettier, and Husky for code linting, formatting, and pre-commit hooks. Integrated Tailwind CSS and set up TypeScript support. Improved SEO with next-seo. Included initial test setup with Jest.

--- blogsphere-pro/babel.config.js
+++ blogsphere-pro/babel.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-    presets: ['next/babel']
+    presets: ['next/babel'],
+    plugins: [
+      ['module-resolver', {
+        alias: {
+          '@components': './src/components',
+          '@styles': './src/styles',
+        },
+      }],
+    ],
   }
   
---
+++ blogsphere-pro/next-sitemap.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    siteUrl: 'https://www.yourblog.com',
+    generateRobotsTxt: true,
+};
