ea27cc4608b31573accd51f5811c3b5390e7180b
duartium <byronduarte95@gmail.com>
2024-11-09 00:04:41 -0500

    🔥 feat: astro base version

--- blogsphere-pro/src/components/home-page/featured-posts.js
+++
@@ -1,8 +0,0 @@
-import classes from './featured-posts.module.css'
-import PostsGrid from '../posts/posts-grid';
-export default function FeaturedPosts(props){
-    return (<section className={classes.latest}>
-        <h2>FEATURE POSTS</h2>
-        <PostsGrid posts={props.posts}/>
-    </section>)
-}
--- blogsphere-pro/src/pages/contact.js
+++
@@ -1,3 +0,0 @@
-function ContactPage () {}
-
-export default ContactPage;
